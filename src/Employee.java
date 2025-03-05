c22c4fe6142371ff189d7d1373b9b956f16fd509
Serafim Nascimento <serafim.snj@gmail.com>
2025-03-05 13:42:47 -0800

    Getters and Setters were coded for the instance variable in the abstract class Employee.

--- src/Employee.java
+++ src/Employee.java
@@ -4,9 +4,33 @@ abstract class Employee {
     protected double salary;
 
     Employee(String name, int id, double salary) {
+        setName(name);
+        setId(id);
+        setSalary(salary);
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 
     public abstract double calculateBonus();
@@ -14,9 +38,9 @@ abstract class Employee {
     public void displayInfo() {
         System.out.println("-----------------------------");
         System.out.println("Employee Details:");
-        System.out.println("Name: " + name);
-        System.out.println("ID: " + id);
-        System.out.println("Salary: " + salary);
+        System.out.println("Name: " + getName());
+        System.out.println("ID: " + getId());
+        System.out.println("Salary: " + getSalary());
         System.out.println("Bonus: " + calculateBonus());
         System.out.println("-----------------------------");
     }
